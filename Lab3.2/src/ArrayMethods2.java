d7ddd94fa719a82e34e4ea4e1db002654e45c525
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-02-05 09:21:35 -0500

    dfghe

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -1,5 +1,34 @@
+//Cristina Chen
+//Lab 3.2 Merge and Partition
 
 public class ArrayMethods2 
 {
-yayy
+	//Pre-Condition: Both list1 and list 2 contain Strings in alphabetical order from a to z
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		//array for each, an empty array that gets filled in
+		//psuedo code, start off with both index 0 
+		//then compare, whatever is less puts into new array
+		//then ++ the less one
+		int o = list1.length;
+		int p = list2.length;
+		new String[] final = {};
+		for(int i = 0; i < o - 1; i++)			//first list?
+		{
+			for(int j = 0; j < p - 1; j++)		//second list?
+			{
+				if(list2[j].compareTo(list1[i]) > 0)
+				{
+					
+				}
+			}
+		}
+	}
+	
+	
+	
+	//public static int partition(int[] list)
+	//{
+		
+	//}
 }
